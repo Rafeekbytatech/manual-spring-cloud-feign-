04aa16c7b7b57215d431ecfebfeb4deacece9fb4
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-08-30 12:27:36 +0530

    7 commit

--- src/main/java/com/bytatech/ayoos/client/OAuth2InterceptedFeignConfiguration.java
+++ src/main/java/com/bytatech/ayoos/client/OAuth2InterceptedFeignConfiguration.java
@@ -3,11 +3,12 @@ package com.bytatech.ayoos.client;
 import java.io.IOException;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import feign.RequestInterceptor;
 
 import com.bytatech.ayoos.security.oauth2.AuthorizationHeaderUtil;
-
+@Configuration
 public class OAuth2InterceptedFeignConfiguration {
 
     @Bean(name = "oauth2RequestInterceptor")
