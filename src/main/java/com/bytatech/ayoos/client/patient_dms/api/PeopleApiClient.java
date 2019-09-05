8fe871dc85c60e16e29a0df3ccf241b57da92593
rafeek <abdul.rafeek@lxisoft.com>
2019-09-05 12:14:26 +0530

    Update PeopleApiClient.java

--- src/main/java/com/bytatech/ayoos/client/patient_dms/api/PeopleApiClient.java
+++ src/main/java/com/bytatech/ayoos/client/patient_dms/api/PeopleApiClient.java
@@ -5,6 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 
 import com.bytatech.ayoos.client.patient_dms.ClientConfiguration;
 
-@FeignClient(name = "${patientDMS.name:patientDMS}", url = "${patientDMS.url:http://34.74.192.113:8082/alfresco/api/-default-/public/alfresco/versions/1}", configuration = ClientConfiguration.class)
+@FeignClient(name = "${patientDMS.name:patientDMS}", url = "${patientDMS.url:https://tohpih.trial.alfresco.com/alfresco/api/-default-/public/alfresco/versions/1}", configuration = ClientConfiguration.class)
 public interface PeopleApiClient extends PeopleApi {
 }
