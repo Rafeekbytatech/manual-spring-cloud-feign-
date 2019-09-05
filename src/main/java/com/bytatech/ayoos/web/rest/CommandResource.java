7e5cf188c0e9898d542882b27d0a3792f4431d66
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-09-05 14:02:42 +0530

    refactored

--- src/main/java/com/bytatech/ayoos/web/rest/CommandResource.java
+++ src/main/java/com/bytatech/ayoos/web/rest/CommandResource.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -75,7 +77,7 @@ public class CommandResource {
 	  ENTITY_NAME, "idexists"); }
 	  createPersonOnDMS(patientDTO);
 	  
-	  SitesApi site=getSiteApiClient(abdul.rafeek@lxisoft.com,tvd600kros);
+	  SitesApi site=getSiteApiClient("abdul.rafeek@lxisoft.com","tvd600kros");
 	  
 	  String siteId= patientDTO.getPatientCode()+"site";
 	  String dmsId = createSite(site,siteId);
@@ -85,76 +87,7 @@ public class CommandResource {
 	  return ResponseEntity.ok().headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME,
 	  result.getId().toString())) .body(result); }
 
-	/**
-	 * PUT /patients : Updates an existing patient.
-	 *
-	 * @param patientDTO
-	 *            the patientDTO to update
-	 * @return the ResponseEntity with status 200 (OK) and with body the updated
-	 *         patientDTO, or with status 400 (Bad Request) if the patientDTO is not
-	 *         valid, or with status 500 (Internal Server Error) if the patientDTO
-	 *         couldn't be updated
-	 * @throws URISyntaxException
-	 *             if the Location URI syntax is incorrect
-	 */
-	/*
-	 * @PutMapping("/patients") public ResponseEntity<PatientDTO>
-	 * updatePatient(@RequestBody PatientDTO patientDTO) throws URISyntaxException {
-	 * log.debug("REST request to update Patient : {}", patientDTO); if
-	 * (patientDTO.getId() == null) { throw new
-	 * BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull"); } PatientDTO
-	 * result = commandService.save(patientDTO); return ResponseEntity.ok()
-	 * .headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME,
-	 * patientDTO.getId().toString())).body(result); }
-	 */
-
-	/**
-	 * POST /address-lines : Create a new addressLine.
-	 *
-	 * @param addressLineDTO
-	 *            the addressLineDTO to create
-	 * @return the ResponseEntity with status 201 (Created) and with body the new
-	 *         addressLineDTO, or with status 400 (Bad Request) if the addressLine
-	 *         has already an ID
-	 * @throws URISyntaxException
-	 *             if the Location URI syntax is incorrect
-	 */
-	/*
-	 * @PostMapping("/address-lines") public ResponseEntity<AddressLineDTO>
-	 * createAddressLine(@RequestBody AddressLineDTO addressLineDTO) throws
-	 * URISyntaxException { log.debug("REST request to save AddressLine : {}",
-	 * addressLineDTO); if (addressLineDTO.getId() != null) { throw new
-	 * BadRequestAlertException("A new addressLine cannot already have an ID",
-	 * ENTITY_NAME, "idexists"); } AddressLineDTO resultDTO =
-	 * addressLineService.save(addressLineDTO); if (resultDTO.getId() == null) {
-	 * throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull"); }
-	 * AddressLineDTO result = addressLineService.save(resultDTO); return
-	 * ResponseEntity.ok().headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME,
-	 * result.getId().toString())) .body(result); }
-	 */
-	/**
-	 * PUT /address-lines : Updates an existing addressLine.
-	 *
-	 * @param addressLineDTO
-	 *            the addressLineDTO to update
-	 * @return the ResponseEntity with status 200 (OK) and with body the updated
-	 *         addressLineDTO, or with status 400 (Bad Request) if the
-	 *         addressLineDTO is not valid, or with status 500 (Internal Server
-	 *         Error) if the addressLineDTO couldn't be updated
-	 * @throws URISyntaxException
-	 *             if the Location URI syntax is incorrect
-	 */
-	/*
-	 * @PutMapping("/address-lines") public ResponseEntity<AddressLineDTO>
-	 * updateAddressLine(@RequestBody AddressLineDTO addressLineDTO) throws
-	 * URISyntaxException { log.debug("REST request to update AddressLine : {}",
-	 * addressLineDTO); if (addressLineDTO.getId() == null) { throw new
-	 * BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull"); }
-	 * AddressLineDTO result = addressLineService.save(addressLineDTO); return
-	 * ResponseEntity.ok() .headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME,
-	 * addressLineDTO.getId().toString())) .body(result); }
-	 */
-
+	
 	public void createPersonOnDMS(PatientDTO patientDTO) {
 		log.debug("=================into the process createPeople()===========");
 
@@ -172,6 +105,15 @@ public class CommandResource {
 		return peopleApi.getPerson(patientCode, null).getBody();
 	}
 
+	
+	
+	@GetMapping("/test/{s}")
+	public void Test(@PathVariable String s) {
+		 SitesApi site= getSiteApiClient("abdul.rafeek@lxisoft.com","tvd600kros");
+		createSite(site,s);
+	}
+	
+	
 	public SitesApi getSiteApiClient(String userName, String password) {
 		SitesApi sitesApi = Feign.builder().encoder(encoder).decoder(decoder)
 				.requestInterceptor(new BasicAuthRequestInterceptor("userName", "password"))
@@ -180,13 +122,6 @@ public class CommandResource {
 		return sitesApi;
 	}
 
-	/**
-	 * Create a new patientDMS-Site.
-	 *
-	 * @param siteId
-	 *            the patientDMS-Site to create
-	 *
-	 */
 
 	public String createSite(SitesApi site,String siteId) {
 		SiteBodyCreate siteBodyCreate = new SiteBodyCreate();
